50c2dc9e5e90c7c00ff7dfe6dac61f0ff499d6be
ankitkachru <ankitkachru@gmail.com>
2016-04-17 22:20:26 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 
-An example app for buidling an API with Django REST framework
+API using Django REST
--- rest_example/urls.py
+++ rest_example/urls.py
@@ -3,7 +3,32 @@ from django.conf.urls import patterns, include, url
 from restapp import views
 
 from django.contrib import admin
+
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
 admin.autodiscover()
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,4 +38,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
+
+
